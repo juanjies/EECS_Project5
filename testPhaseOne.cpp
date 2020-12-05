75208498771f9d32e2e3dd6bc60a43cc11126677
Juan <64536309+juanjies@users.noreply.github.com>
2020-12-04 21:43:05 -0500

    Add files via upload

--- testPhaseOne.cpp
+++ testPhaseOne.cpp
@@ -4,5 +4,7 @@
 int main() {
   LinkedNodeClass< int > intLNC(NULL, 978, NULL);
   cout << intLNC.getValue() << endl;
+  LinkedNodeClass< float > decimalLNC(NULL, 3.14, NULL);
+  cout << decimalLNC.getValue() << endl;
   return 0;
 }
