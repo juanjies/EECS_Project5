662d9a11b5b0641fc1f28fdd87ad3f4f58ef7cf2
Juan <64536309+juanjies@users.noreply.github.com>
2020-12-05 19:24:29 -0500

    Add files via upload

--- testPhaseOne.cpp
+++ testPhaseOne.cpp
@@ -10,17 +10,14 @@ int main() {
   EventClass eventOne(1, EVENT_ARRIVE_EAST);
   EventClass eventTwo(2, EVENT_ARRIVE_WEST);
 
-  LinkedNodeClass< EventClass > intLNCOne(NULL, eventOne, NULL);
-  LinkedNodeClass< EventClass > intLNCTwo(NULL, eventTwo,NULL);
+  LinkedNodeClass < EventClass > LNCOne(NULL, eventOne, NULL);
+  LinkedNodeClass < EventClass > LNCTwo(NULL, eventTwo,NULL);
   
   // cout << intLNCOne.getValue() << endl;
-  cout << eventOne;
+  cout << LNCOne.getValue() << endl;
+  cout << LNCTwo.getValue() << endl;
 
-  SortedListClass< int > intSLC;
-  intSLC.insertValue(5);
-  intSLC.insertValue(9);
-  intSLC.insertValue(7);
-  intSLC.insertValue(8);
-  intSLC.printForward();
+  // SortedListClass< EventClass > SLCOne;
+  // SLCOne.insertValue(5);
   return 0;
 }
